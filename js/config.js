38d4b7bf39bece34328fa549f44e4f9742c40cf8
likithkumarem86181 <149880936+likithkumarem86181@users.noreply.github.com>
2025-03-08 15:51:41 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_E0UHgfIgD', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '73e2q14sp4tmqvg3ds02eac7ec', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        ClientSecret:'bin92pjgihqldimte6l5quojr54nn92c74m1mink7cpqbmquatn',
-        region: 'ap-south-1' // e.g. us-east-2
+        userPoolId: 'ap-south-1_E0UHgfIgD', // Replace with your actual User Pool ID
+        userPoolClientId: '73e2q14sp4tmqvg3ds02eac7ec', // Replace with your App Client ID
+        clientSecret: 'bin92pjgihqldimte6l5quojr54nn92c74m1mink7cpqbmquatn', // 🔥 Add your Cognito Client Secret here!
+        region: 'ap-south-1' // Your AWS Region
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://your-api-url
     }
 };
