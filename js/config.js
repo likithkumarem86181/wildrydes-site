f1b47de76960c7008f6a1591153e038bfc22d831
likithkumarem86181 <149880936+likithkumarem86181@users.noreply.github.com>
2025-05-18 23:12:35 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_jDPkrfJOm', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '75c1bpcn0v8754mvgbmomltip4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_sWf28m29o', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6ppp91e20t7ekc01a5p4sdkvoq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
